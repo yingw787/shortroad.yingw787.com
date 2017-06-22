43d3cdb0ddc5b7550ae6f972b3e2f4f129b02fb4
Ying Wang <yingw787@gmail.com>
2017-06-22 00:34:53 -0400

    Successful s3 deploy and reconfiguring title.

--- s3_deploy.sh
+++ s3_deploy.sh
@@ -1,4 +1,4 @@
 rm -rf public
 hugo --verbose
-aws s3 rm s3://air.yingw787.com --recursive --profile s3_personal
-aws s3 cp ./public s3://air.yingw787.com --recursive --profile s3_personal
+aws s3 rm s3://shortroad.yingw787.com --recursive --profile s3_personal
+aws s3 cp ./public s3://shortroad.yingw787.com --recursive --profile s3_personal
